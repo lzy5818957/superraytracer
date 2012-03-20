5eb3bfc5e1b5339afe651b9a32537e1df5573f95
fugitive722@gmail.com <fugitive722@gmail.com@387fecb7-b4ad-9c71-8f46-2f10ff65b5a9>
2012-03-20 01:40:09 +0000

    Add cudaVectUtil

---
+++ trunk/superraytracer/src/Util/cudaVectUtil.cuh
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef __INC_CUDA_VECTOR_UTIL_H__
+#define __INC_CUDA_VECTOR_UTIL_H__
+
+#include "../GML/gml.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+__device__ float* vector3ToArray(gml::vec3_t vec);
+
+__device__ gml::vec3_t arrayToVector3(float* array);
+
+__device__ float* matrix4x4ToArray(gml::mat4x4_t mat);
+
+__device__ gml::mat4x4_t arrayToMatrix4x4(float* array);
+
+#endif
