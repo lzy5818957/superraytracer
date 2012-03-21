8bfa7c7bef7211e54f1e5783f6dc28fea7650426
lzy5818957 <lzy5818957@387fecb7-b4ad-9c71-8f46-2f10ff65b5a9>
2012-03-21 04:41:56 +0000

    added functions

--- trunk/superraytracer/src/Util/cudaVectUtil.cuh
+++ trunk/superraytracer/src/Util/cudaVectUtil.cuh
@@ -6,12 +6,8 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-__device__ float* vector3ToArray(gml::vec3_t vec);
+__device__ float* Mat4x4_Mul_Mat4x4(float *A, float *B, float *c);
+__device__ float* Mat4x4_Mul_Vec4(float *A, float *B, float *c);
 
-__device__ gml::vec3_t arrayToVector3(float* array);
-
-__device__ float* matrix4x4ToArray(gml::mat4x4_t mat);
-
-__device__ gml::mat4x4_t arrayToMatrix4x4(float* array);
 
 #endif
