b5b7d60bdb8d93727dc752616efc456949e50e7b
lzy5818957 <lzy5818957@387fecb7-b4ad-9c71-8f46-2f10ff65b5a9>
2012-03-22 05:06:06 +0000

    fixed some bugs

--- trunk/superraytracer/src/Objects/Models/plane.h
+++ trunk/superraytracer/src/Objects/Models/plane.h
@@ -1,24 +1,24 @@
 
 /*
- * Copyright:
- * Daniel D. Neilson (ddneilson@ieee.org)
- * University of Saskatchewan
- * All rights reserved
- *
- * Permission granted to use for use in assignments and
- * projects for CMPT 485 & CMPT 829 at the University
- * of Saskatchewan.
- */
+* Copyright:
+* Daniel D. Neilson (ddneilson@ieee.org)
+* University of Saskatchewan
+* All rights reserved
+*
+* Permission granted to use for use in assignments and
+* projects for CMPT 485 & CMPT 829 at the University
+* of Saskatchewan.
+*/
 
 /*
- * Geometry for a plane
- *
- * The plane is centered at (0,0,0), and is entirely
- * in the xz plane. Coordinates of: x,z \in [-1,1]
- *
- * The front face of the plane is in the +y-axis direction.
- * The normal to the plane is the y-axis.
- */
+* Geometry for a plane
+*
+* The plane is centered at (0,0,0), and is entirely
+* in the xz plane. Coordinates of: x,z \in [-1,1]
+*
+* The front face of the plane is in the +y-axis direction.
+* The normal to the plane is the y-axis.
+*/
 
 #pragma once
 #ifndef __INC_PLANE_H_
@@ -29,27 +29,29 @@
 
 namespace Object
 {
-namespace Models
-{
+	namespace Models
+	{
 
-class Plane : public Geometry
-{
-protected:
-	Mesh m_mesh;
-public:
-	Plane();
-	~Plane();
+		class Plane : public Geometry
+		{
+		protected:
+			Mesh m_mesh;
+		public:
+			Plane();
+			~Plane();
 
-	bool init();
+			bool init();
 
-	virtual void rasterize() const;
+			virtual void rasterize() const;
 
-	virtual bool rayIntersects(const RayTracing::Ray_t &ray, const float t0, const float t1, RayTracing::HitInfo_t &hitinfo) const;
-	virtual bool shadowsRay(const RayTracing::Ray_t &ray, const float t0, const float t1) const;
-	virtual void hitProperties(const RayTracing::HitInfo_t &hitinfo, gml::vec3_t &normal, gml::vec2_t &texCoords) const;
-};
+			virtual bool rayIntersects(const RayTracing::Ray_t &ray, const float t0, const float t1, RayTracing::HitInfo_t &hitinfo) const;
+			virtual bool shadowsRay(const RayTracing::Ray_t &ray, const float t0, const float t1) const;
+			virtual void hitProperties(const RayTracing::HitInfo_t &hitinfo, gml::vec3_t &normal, gml::vec2_t &texCoords) const;
+			RayTracing::HitInfo_t* Plane::rayIntersectsWithCuda(const RayTracing::Ray_t *rays, const float t0, const float t1)const;
 
-}
+		};
+
+	}
 }
 
 #endif
