18e4be95bf486f2fa56d1d0fa856fc83dd0a96ae
lzy5818957 <lzy5818957@387fecb7-b4ad-9c71-8f46-2f10ff65b5a9>
2012-03-22 03:41:28 +0000

    added code for fu

--- trunk/superraytracer/src/Objects/Models/plane_kernel.cu
+++ trunk/superraytracer/src/Objects/Models/plane_kernel.cu
@@ -5,6 +5,46 @@
 
 __global__ void raysIntersectsPlaneKernel(float *devRays, const float t0, const float t1, const int w, const int h, RayTracing::HitInfo_t *hitInfos)
 {
+	gml::vec3_t E1(0.0f, 0.0f, 2.0f);
+	gml::vec3_t E2(2.0f, 0.0f, 0.0f);
+
+	gml::vec3_t P = gml::cross( ray.d, E2 );
+
+	float detM = gml::dot(P, E1);
+
+	if (fabs(detM) < 1e-4)
+	{
+		return false;
+	}
+
+	gml::vec3_t T = gml::sub( ray.o, _verts[0] );
+
+	float u = gml::dot( P, T ) / detM;
+	
+	if ( u < 0.0f || 1.0f < u )
+	{
+		return false;
+	}
+
+	gml::vec3_t TxE1 = gml::cross(T, E1);
+	float v = gml::dot( TxE1, ray.d ) / detM;
+	if ( v < 0.0f || 1.0f < v)
+	{
+		return false;
+	}
+
+	float t = gml::dot( TxE1, E2 ) / detM;
+	if (t < t0 || t1 < t)
+	{
+		return false;
+	}
+
+	hitinfo.hitDist =  t;
+  
+	hitinfo.plane.u = u;
+	hitinfo.plane.v = v;
+	
+	return true;
 
 
 }
@@ -53,6 +93,6 @@ extern "C" cudaError_t raysIntersectsWithCudaPlane(float *devRays, const float t
 	devRays = 0;
 
 Error:
-	//cudaFree(devRayDirs);
+	cudaFree(devHitInfos);
 	return cudaStatus;
 }
