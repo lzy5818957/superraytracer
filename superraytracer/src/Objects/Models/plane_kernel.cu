26196caa270a0c12d9fbf96e040ba4ce6527638c
lzy5818957 <lzy5818957@387fecb7-b4ad-9c71-8f46-2f10ff65b5a9>
2012-03-22 03:18:53 +0000

    shere_kernel

--- superraytracer/src/Objects/Models/plane_kernel.cu
+++ superraytracer/src/Objects/Models/plane_kernel.cu
@@ -1,7 +1,27 @@
 #include "plane_kernel.cuh"
+#include <cstdio>
 
-
-extern "C" cudaError_t raysIntersectsWithCudaPlane(float *devRays, const float t0, const float t1, RayTracing::HitInfo_t *hitinfos)
+extern "C" cudaError_t raysIntersectsWithCudaPlane(float *devRays, const float t0, const float t1, const int w, const int h, RayTracing::HitInfo_t *hostHitInfos)
 {
+	RayTracing::HitInfo_t *devHitInfos = 0;
+	cudaError_t cudaStatus;
 
+	// Choose which GPU to run on, change this on a multi-GPU system.
+	cudaStatus = cudaSetDevice(0);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+		goto Error;
+	}
+
+
+	cudaStatus = cudaMalloc (( void **)& devHitInfos , w * h * sizeof ( float ));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		goto Error;
+	}
+
+
+Error:
+	//cudaFree(devRayDirs);
+	return cudaStatus;
 }
